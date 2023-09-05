b9b913526705dd92f38df3cb8f6ee1932ea8beea
Nazlıcan Çay <nazlican1306@gmail.com>
2023-09-05 11:24:39 +0300

    Initial commit

---
+++ task4_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task4_app
+//
+//  Created by Nazlıcan Çay on 5.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
